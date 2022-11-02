014f1514e638cdd9cac00acad643b11778348b69
wyf043277 <2423839638@qq.com>
2022-11-02 19:44:14 +0800

    人资项目初始化

---
+++ src/permission.js
@@ -0,0 +1,16 @@
+import router from './router'
+import NProgress from 'nprogress' // progress bar
+import 'nprogress/nprogress.css' // progress bar style
+
+NProgress.configure({ showSpinner: false }) // NProgress Configuration
+
+const whiteList = ['/login'] // no redirect whitelist
+
+router.beforeEach(async(to, from, next) => {
+  next()
+})
+
+router.afterEach(() => {
+  // finish progress bar
+  NProgress.done()
+})
---
+++ src/utils/request.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import { MessageBox, Message } from 'element-ui'
+import store from '@/store'
+
+// create an axios instance
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API, 
+  timeout: 5000
+})
+
+service.interceptors.request.use(
+  config => {
+    return config
+  },
+  error => {
+    console.log(error) // for debug
+    return Promise.reject(error)
+  }
+)
+
+// response interceptor
+service.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+export default service
