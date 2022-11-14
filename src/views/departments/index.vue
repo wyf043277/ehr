e0899600d6746304d0d652d61148f250f32b6be3
wyf043277 <2423839638@qq.com>
2022-11-14 23:00:49 +0800

    20221114 街上

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -3,52 +3,52 @@
     <div class="app-container">
       <el-card>
         <div slot="header">
-          <el-tabs value='first' @tab-click="handleClick">
-              <el-tab-pane label="组织导航" name="first" class="tab-pane">
+          <el-tabs value="first" @tab-click="handleClick">
+            <el-tab-pane label="组织导航" name="first" class="tab-pane">
 
-                            <el-row type="flex" justify="space-between" align="middle" class="department-header">
-                              <!-- 第一列20份 -->
-                              <el-col :span="20">
-                                <i class="el-icon-s-home" />
-                                <span class="company">江苏WYF股份有限公司</span>
-                              </el-col>
-                              <!-- 第二列4份 -->
-                              <el-col :span="4">
-                                <!-- 在容器范围里, 一行再分2列 -->
-                                <el-row type="flex" justify="end">
-                                  <el-col>负责人</el-col>
-                                  <el-col>
-                                    <!-- 这个之前在头部导航右侧下拉菜单讲过 -->
-                                    <el-dropdown>
-                                      <!-- 下拉菜单文字 -->
-                                      <span class="el-dropdown-link">
-                                        操作<i class="el-icon-arrow-down el-icon--right" />
-                                      </span>
-                                      <!-- 下拉项 -->
-                                      <el-dropdown-menu slot="dropdown">
-                                        <el-dropdown-item>添加子部门</el-dropdown-item>
-                                      </el-dropdown-menu>
-                                    </el-dropdown>
-                                  </el-col>
-                                </el-row>
-                              </el-col>
-                            </el-row>
-                          </el-tab-pane>
-            </el-tabs>
+              <el-row type="flex" justify="space-between" align="middle" class="department-header">
+                <!-- 第一列20份 -->
+                <el-col :span="20">
+                  <i class="el-icon-s-home" />
+                  <span class="company">江苏WYF股份有限公司</span>
+                </el-col>
+                <!-- 第二列4份 -->
+                <el-col :span="4">
+                  <!-- 在容器范围里, 一行再分2列 -->
+                  <el-row type="flex" justify="end">
+                    <el-col>负责人</el-col>
+                    <el-col>
+                      <!-- 这个之前在头部导航右侧下拉菜单讲过 -->
+                      <el-dropdown>
+                        <!-- 下拉菜单文字 -->
+                        <span class="el-dropdown-link">
+                          操作<i class="el-icon-arrow-down el-icon--right" />
+                        </span>
+                        <!-- 下拉项 -->
+                        <el-dropdown-menu slot="dropdown">
+                          <el-dropdown-item>添加子部门</el-dropdown-item>
+                        </el-dropdown-menu>
+                      </el-dropdown>
+                    </el-col>
+                  </el-row>
+                </el-col>
+              </el-row>
+            </el-tab-pane>
+          </el-tabs>
         </div>
-        <el-tree :props="defaultProps" :data="data"  node-key="id" default-expand-all :expand-on-click-node="false" indent=2>
+        <el-tree :props="defaultProps" :data="data" node-key="id" default-expand-all :expand-on-click-node="false" class="tree">
           <template slot-scope="{node,data}">
-            <el-row type="flex" justify="space-between" align="middle" class="department-header" style="height: 40px; width: 100%;">
+            <div class="department-wrap">
               <!-- 第一列20份 -->
-              <el-col :span="20">
-                <span class="company">{{node.label}}</span>
-              </el-col>
+              <div>
+                <span class="company">{{ node.label }}</span>
+              </div>
               <!-- 第二列4份 -->
-              <el-col :span="4">
+              <div class="interactive">
                 <!-- 在容器范围里, 一行再分2列 -->
-                <el-row type="flex" justify="end">
-                  <el-col>负责人</el-col>
-                  <el-col>
+                <div>
+                  <div>负责人</div>
+                  <div>
                     <el-dropdown>
                       <!-- 下拉菜单文字 -->
                       <span class="el-dropdown-link">
@@ -61,10 +61,10 @@
                         <el-dropdown-item>删除部门</el-dropdown-item>
                       </el-dropdown-menu>
                     </el-dropdown>
-                  </el-col>
-                </el-row>
-              </el-col>
-            </el-row>
+                  </div>
+                </div>
+              </div>
+            </div>
           </template>
         </el-tree>
       </el-card>
@@ -73,51 +73,51 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        data:[{
-          label: '一级 1',
+export default {
+  data() {
+    return {
+      data: [{
+        label: '一级 1',
+        children: [{
+          label: '二级 1-1',
           children: [{
-            label: '二级 1-1',
-            children: [{
-              label: '三级 1-1-1'
-            }]
+            label: '三级 1-1-1'
+          }]
+        }]
+      }, {
+        label: '一级 2',
+        children: [{
+          label: '二级 2-1',
+          children: [{
+            label: '三级 2-1-1'
           }]
         }, {
-          label: '一级 2',
+          label: '二级 2-2',
           children: [{
-            label: '二级 2-1',
-            children: [{
-              label: '三级 2-1-1'
-            }]
-          }, {
-            label: '二级 2-2',
-            children: [{
-              label: '三级 2-2-1'
-            }]
+            label: '三级 2-2-1'
+          }]
+        }]
+      }, {
+        label: '一级 3',
+        children: [{
+          label: '二级 3-1',
+          children: [{
+            label: '三级 3-1-1'
           }]
         }, {
-          label: '一级 3',
+          label: '二级 3-2',
           children: [{
-            label: '二级 3-1',
-            children: [{
-              label: '三级 3-1-1'
-            }]
-          }, {
-            label: '二级 3-2',
-            children: [{
-              label: '三级 3-2-1'
-            }]
+            label: '三级 3-2-1'
           }]
-        }],
-        defaultProps: {
-          children: 'children',
-          label: 'label'
-        }
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
       }
-    },
+    }
   }
+}
 </script>
 
 <style scoped lang="scss">
@@ -139,6 +139,20 @@
       display: flex;
       align-items: center;
       border-bottom: 1px solid #ccc;
+    }
+  }
+  .tree{
+    padding: 0 80px;
+  }
+  .department-wrap{
+    width: 100%;
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .interactive{
+      display: flex;
+      width: 300px;
     }
   }
 </style>
