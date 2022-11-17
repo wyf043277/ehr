cd462f450542af36e5b441a2eb31cac7f7d5eb59
wyf043277 <2423839638@qq.com>
2022-11-17 23:30:49 +0800

    20221117 街上

--- src/views/departments/components/departDialog.vue
+++ src/views/departments/components/departDialog.vue
@@ -1,10 +1,37 @@
 <template>
   <div>
-    <span>这是一段信息</span>
+    <el-form ref="deptForm" label-width="120px">
+      <el-form-item label="部门名称">
+        <el-input v-model="form.name" style="width: 80%;" placeholder="1-50个字符" />
+      </el-form-item>
+      <el-form-item label="部门编码">
+        <el-input v-model="form.code" style="width: 80%;" placeholder="1-50个字符" />
+      </el-form-item>
+      <el-form-item label="部门负责人">
+        <el-select v-model="form.manager" style="width: 80%;" placeholder="1-50个字符">
+          <el-option />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="部门介绍">
+        <el-input v-model="form.introduce" style="width: 80%;" placeholder="1-300个字符" type="textarea" :rows="3" />
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      form: {
+        name: '', // 部门名称
+        code: '', // 部门编码
+        manager: '', // 部门负责人
+        introduce: ''// 部门介绍
+      }
+    }
+  }
+}
 </script>
 
 <style>
