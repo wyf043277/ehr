44a4919f84185f1ef454bc62ba9afeb8d003bdf8
wyf043277 <2423839638@qq.com>
2022-11-26 12:59:47 +0800

    20221126 街上

---
+++ src/components/PageTools/ActionBox.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-card>
+    <div class="page-tools">
+      <!-- 左侧 -->
+      <div class="left">
+        <div class="tips">
+          <i class="el-icon-info" />
+          <slot name="slot-left">
+            <span>文字区域</span>
+          </slot>
+        </div>
+      </div>
+
+      <div class="right">
+        <!-- 右侧 -->
+        <slot name="slot-right">
+          <span>按钮区域</span>
+        </slot>
+      </div>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.page-tools {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .tips {
+    line-height: 34px;
+    padding: 0px 15px;
+    border-radius: 5px;
+    border: 1px solid rgba(145, 213, 255, 1);
+    background: rgba(230, 247, 255, 1);
+
+    i {
+      margin-right: 10px;
+      color: #409eff;
+    }
+  }
+}
+</style>
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="dashboard-container">
-    <div class="app-container">
-      <h2>员工</h2>
-    </div>
+    <div class="app-container" />
+    <ActionBox>
+      <template slot="slot-left">
+        <span>哈哈哈</span>
+      </template>
+      <template slot="slot-right">
+        <el-button type="danger" size="small">导入excel</el-button>
+        <el-button type="success" size="small">导出excel</el-button>
+        <el-button type="primary" size="small">新增员工</el-button>
+      </template>
+    </ActionBox>
   </div>
 </template>
 
 <script>
+import ActionBox from '@/components/PageTools/ActionBox'
+export default {
+  components: {
+    ActionBox
+  }
+}
 </script>
 
 <style>
