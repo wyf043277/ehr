e79f5e6ebb85e70acf6eb71ca2a4b879a3507268
wyf043277 <2423839638@qq.com>
2022-11-28 21:56:30 +0800

    20221128 街上

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -9,11 +9,11 @@
         :before-close="handleClose"
         @close="dialogClose"
       >
-      <departDialog ref="departDialog" :EmpolyeesSimpleList="EmpolyeesSimpleList" :editData="editData" :departmentFlat="departmentFlat"></departDialog>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="dialogCancel">取 消</el-button>
-        <el-button type="primary" @click="dialogConfirm">确 定</el-button>
-      </span>
+        <departDialog ref="departDialog" :empolyees-simple-list="EmpolyeesSimpleList" :edit-data="editData" :department-flat="departmentFlat" />
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogCancel">取 消</el-button>
+          <el-button type="primary" @click="dialogConfirm">确 定</el-button>
+        </span>
       </el-dialog>
       <el-card>
         <div slot="header">
@@ -40,7 +40,7 @@
                         </span>
                         <!-- 下拉项 -->
                         <el-dropdown-menu slot="dropdown">
-                          <el-dropdown-item @click.native='add({"id":""})'>添加子部门</el-dropdown-item>
+                          <el-dropdown-item @click.native="add({&quot;id&quot;:&quot;&quot;})">添加子部门</el-dropdown-item>
                         </el-dropdown-menu>
                       </el-dropdown>
                     </div>
@@ -69,7 +69,7 @@
                             <el-dropdown-menu slot="dropdown">
                               <el-dropdown-item @click.native="add(data)">添加子部门</el-dropdown-item>
                               <el-dropdown-item @click.native="edit(data)">编辑部门</el-dropdown-item>
-                              <el-dropdown-item @click.native="del(data)" v-if="data && !data.children">删除部门</el-dropdown-item>
+                              <el-dropdown-item v-if="data && !data.children" @click.native="del(data)">删除部门</el-dropdown-item>
                             </el-dropdown-menu>
                           </el-dropdown>
                         </div>
@@ -87,9 +87,12 @@
 </template>
 
 <script>
-import { getDepartmentsListAPI,getEmpolyeesSimpleListAPI,addDepartmentAPI,getDepartmentAPI,editDepartmentAPI,delDepartmentAPI} from '@/api'
+import { getDepartmentsListAPI, getEmpolyeesSimpleListAPI, addDepartmentAPI, getDepartmentAPI, editDepartmentAPI, delDepartmentAPI } from '@/api'
 import departDialog from './components/departDialog.vue'
 export default {
+  components: {
+    departDialog
+  },
   data() {
     return {
       data: [],
@@ -98,23 +101,23 @@ export default {
         label: 'name'
       },
       dialogVisible: false,
-      EmpolyeesSimpleList:[],
-      pid:'',//添加部门的父级id
-      editData:{},//编辑部门复显
-      isEdit:false ,//判断是否编辑部门
-      departmentFlat:[]//所有部门的扁平化信息
+      EmpolyeesSimpleList: [],
+      pid: '', // 添加部门的父级id
+      editData: {}, // 编辑部门复显
+      isEdit: false, // 判断是否编辑部门
+      departmentFlat: []// 所有部门的扁平化信息
     }
-  },
-  components:{
-    departDialog
   },
   computed: {
     treeData() {
       const reg = /部$/
       const temp = this.data.filter(item => {
+        if (item.name.indexOf('黑马') != -1) {
+          return false
+        }
         return reg.test(item.name)
       })
-      this.departmentFlat=temp
+      this.departmentFlat = temp
       return this.handleDepart(temp, '')
     }
   },
@@ -132,7 +135,7 @@ export default {
       }
     },
     handleDepart(arr, pid) {
-      //将扁平数据变得有层级
+      // 将扁平数据变得有层级
       const res = []
       arr.forEach((item) => {
         if (item.pid == pid) {
@@ -147,9 +150,9 @@ export default {
       return res
     },
     handleClose(done) {
-      this.$confirm('确认关闭？','提示',{
+      this.$confirm('确认关闭？', '提示', {
         confirmButtonText: '确定',
-        cancelButtonText: '取消',
+        cancelButtonText: '取消'
       })
         .then(_ => {
           done()
@@ -158,91 +161,90 @@ export default {
     },
     add(data) {
       this.dialogVisible = true
-      this.pid=data.id
-      this.isEdit=false
-      this.editData={pid:data.id}
+      this.pid = data.id
+      this.isEdit = false
+      this.editData = { pid: data.id }
     },
     async edit(data) {
-      try{
-        let res=await getDepartmentAPI(data.id)
-        this.editData=res.data
+      try {
+        const res = await getDepartmentAPI(data.id)
+        this.editData = res.data
         this.dialogVisible = true
-        this.isEdit=true
-      }catch(e){
+        this.isEdit = true
+      } catch (e) {
         console.log(e)
       }
     },
-      del(data) {
-        this.$confirm('确认删除？','提示',{
-          confirmButtonText: '确认',
-          cancelButtonText: '取消',
-        })
-          .then(async _ => {
-            // 调用删除接口
-                const delDepartRes = await delDepartmentAPI(data.id)
-                // 根据状态值, 查看是否删除成功
-                if (!delDepartRes.success) return this.$message.error(delDepartRes.message)
-                // 删除成功需要给用户进行提示
-                this.$message.success(delDepartRes.message)
-                // 删除后需要重新获取当前页面数据
-                this.getDepartmentsList()
-          })
-          .catch(_ => {
-            this.$message("取消删除")
-          })
-
-    },
-    dialogCancel(){
-      this.dialogVisible=false
-    },
-    async dialogConfirm(){
-      this.$refs.departDialog.$refs.deptForm.validate(async (valid)=>{
-        if(valid){
-      if(this.isEdit){
-        let data={...this.$refs.departDialog.form,id:this.editData.id}
-        let res =await editDepartmentAPI(data)
-        if(res.success){
+    del(data) {
+      this.$confirm('确认删除？', '提示', {
+        confirmButtonText: '确认',
+        cancelButtonText: '取消'
+      })
+        .then(async _ => {
+          // 调用删除接口
+          const delDepartRes = await delDepartmentAPI(data.id)
+          // 根据状态值, 查看是否删除成功
+          if (!delDepartRes.success) return this.$message.error(delDepartRes.message)
+          // 删除成功需要给用户进行提示
+          this.$message.success(delDepartRes.message)
+          // 删除后需要重新获取当前页面数据
           this.getDepartmentsList()
-          this.dialogVisible=false
-          this.$message.success("编辑部门成功")
-        }else{
-          this.$message.error("编辑部门失败")
-        }
-      }else{
-        let data={...this.$refs.departDialog.form,pid:this.pid}
-        try{
-          let res =await addDepartmentAPI(data)
-          if(res.success){
-            this.dialogVisible=false
-            this.getDepartmentsList()
-            this.$message.success("添加部门成功")
-          }else{
-            this.$message.success("添加部门失败")
+        })
+        .catch(_ => {
+          this.$message('取消删除')
+        })
+    },
+    dialogCancel() {
+      this.dialogVisible = false
+    },
+    async dialogConfirm() {
+      this.$refs.departDialog.$refs.deptForm.validate(async(valid) => {
+        if (valid) {
+          if (this.isEdit) {
+            const data = { ...this.$refs.departDialog.form, id: this.editData.id }
+            const res = await editDepartmentAPI(data)
+            if (res.success) {
+              this.getDepartmentsList()
+              this.dialogVisible = false
+              this.$message.success('编辑部门成功')
+            } else {
+              this.$message.error('编辑部门失败')
+            }
+          } else {
+            const data = { ...this.$refs.departDialog.form, pid: this.pid }
+            try {
+              const res = await addDepartmentAPI(data)
+              if (res.success) {
+                this.dialogVisible = false
+                this.getDepartmentsList()
+                this.$message.success('添加部门成功')
+              } else {
+                this.$message.success('添加部门失败')
+              }
+            } catch (e) {
+              console.log(e)
+            }
           }
-        }catch(e){
-          console.log(e)
-        }
-      }
         }
       })
     },
-    async getEmpolyeesSimpleList(){
-      try{
-        let res = await getEmpolyeesSimpleListAPI()
-        let data = res.data.slice(0,100)
-        this.EmpolyeesSimpleList=data.filter(item=>{
-          if(item.username){
+    async getEmpolyeesSimpleList() {
+      try {
+        const res = await getEmpolyeesSimpleListAPI()
+        const data = res.data.slice(0, 100)
+        this.EmpolyeesSimpleList = data.filter(item => {
+          if (item.username) {
             return true
           }
         })
-      }catch(e){
-        //TODO handle the exception
+      } catch (e) {
+        // TODO handle the exception
         console.log(e)
-        console.log("获取用户列表失败")
+        console.log('获取用户列表失败')
       }
     },
-    dialogClose(){
-      //当弹窗关闭时，清空表单
+    dialogClose() {
+      // 当弹窗关闭时，清空表单
       console.log(this.$refs.departDialog)
       this.$nextTick(() => {
         this.$refs.departDialog.$refs.deptForm.resetFields()
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -26,154 +26,155 @@
       </template>
     </ActionBox>
     <el-card style="margin-top: 10px;">
-            <el-table border stripe style="width: 100%" :data="employeesList">
-              <el-table-column label="序号" type="index"/>
-              <el-table-column label="姓名" prop="username"/>
-              <el-table-column label="头像">
-                <template slot-scope="scope" prop="staffPhoto">
-                  <img :src="scope.row.staffPhoto" alt="" class="staffPhoto" @error="imgError($event)">
-                </template>
-              </el-table-column>
-              <el-table-column label="手机号" prop="mobile"/>
-              <el-table-column label="工号" prop="workNumber" sortable :sort-method="workNumberSort"/>
-              <el-table-column label="聘用形式" prop="formOfEmployment">
-                <template slot-scope="scope">
-                  <span>{{+scope.row.formOfEmployment===1?"正式":(scope.row.formOfEmployment===2?"非正式":"未知")}}</span>
-                </template>
-              </el-table-column>
-              <el-table-column label="部门" prop="departmentName"/>
-              <el-table-column label="入职时间" prop="timeOfEntry"/>
-              <el-table-column label="操作" width="280">
-                <template>
-                  <el-button type="text" size="small">查看</el-button>
-                  <el-button type="text" size="small">分配角色</el-button>
-                  <el-button type="text" size="small">删除</el-button>
-                </template>
-              </el-table-column>
-            </el-table>
-            <!-- 分页组件 -->
-            <el-row type="flex" justify="end" align="middle" style="height: 60px">
-              <!-- 分页区域 -->
-              <el-pagination
-                :current-page="query.page"
-                :page-sizes="[10, 15, 20, 25]"
-                :page-size="query.size"
-                layout="total, sizes, prev, pager, next, jumper"
-                :total="total"
-                @size-change="handleSizeChange"
-                @current-change="handleCurrentChange"
-              />
-            </el-row>
-          </el-card>
+      <el-table border stripe style="width: 100%" :data="employeesList">
+        <el-table-column label="序号" type="index" />
+        <el-table-column label="姓名" prop="username" />
+        <el-table-column label="头像">
+          <template slot-scope="scope" prop="staffPhoto">
+            <img :src="scope.row.staffPhoto" alt="" class="staffPhoto" @error="imgError($event)">
+          </template>
+        </el-table-column>
+        <el-table-column label="手机号" prop="mobile" />
+        <el-table-column label="工号" prop="workNumber" sortable :sort-method="workNumberSort" />
+        <el-table-column label="聘用形式" prop="formOfEmployment">
+          <template slot-scope="scope">
+            <span>{{ +scope.row.formOfEmployment===1?"正式":(+scope.row.formOfEmployment===2?"非正式":"未知") }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="部门" prop="departmentName" />
+        <el-table-column label="入职时间" prop="timeOfEntry" />
+        <el-table-column label="操作" width="280">
+          <template>
+            <el-button type="text" size="small">查看</el-button>
+            <el-button type="text" size="small">分配角色</el-button>
+            <el-button type="text" size="small">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页组件 -->
+      <el-row type="flex" justify="end" align="middle" style="height: 60px">
+        <!-- 分页区域 -->
+        <el-pagination
+          :current-page="query.page"
+          :page-sizes="[10, 15, 20, 25]"
+          :page-size="query.size"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+        />
+      </el-row>
+    </el-card>
   </div>
 </template>
 
 <script>
 import ActionBox from '@/components/PageTools/ActionBox'
-import {getEmployeesAPI,addEmployeesAPI} from '@/api'
+import { getEmployeesAPI, addEmployeesAPI } from '@/api'
 import employeeDialog from './components/employeeDialog.vue'
 export default {
   components: {
     ActionBox,
     employeeDialog
   },
-    data() {
-      return {
-        query: {
-          page: 1, // 页码
-          size: 10 // 每页条数
-        },
-        employeesList: [], // 员工列表
-        total: 0 ,// 数据总条数
-        dialogVisible: false,
-        downloading:false
-      }
-    },
-    beforeMount() {
-        this.getEmployees()
-    },
-    methods: {
-      // 每页显示的条数发生改变时触发
-      handleSizeChange(newSize) {},
-
-      // 当前页面发生改变时触发
-      handleCurrentChange(newPage) {},
-      async getEmployees(){
-        //获取员工列表
-        try{
-          let {data:res} =await getEmployeesAPI(this.query)
-          this.total=res.total
-          this.employeesList=res.rows
-          console.log(res)
-        }catch(e){
-          console.error(e)
-        }
+  data() {
+    return {
+      query: {
+        page: 1, // 页码
+        size: 10 // 每页条数
       },
-      workNumberSort(a,b){
-        console.log(a.workNumber-b.workNumber)
-        return a.workNumber-b.workNumber
-      },
-      imgError(e){
-        e.target.src="https://project-waimai.oss-cn-hangzhou.aliyuncs.com/blog/IMG_1515.PNG"
-      },
-      addEmployee(){
-        this.dialogVisible=true
-      },
-      handleClose(done) {
-        this.$confirm('确认关闭？', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消'
-        })
-          .then(_ => {
-            done()
-          })
-          .catch(_ => {})
-      },
-      dialogClose() {
-        // 当弹窗关闭时，清空表单
-        this.$nextTick(() => {
-          this.$refs.employeeDialog.$refs.employeeForm.resetFields()
-        })
-      },
-      dialogCancel() {
-        // 弹窗取消按钮触发
-        this.dialogVisible = false
-      },
-      async dialogConfirm() {
-          // 弹窗确认按钮触发
-          console.log(1)
-          this.$refs.employeeDialog.$refs.employeeForm.validate(async valid => {
-            if (valid) {
-              let res = await addEmployeesAPI(this.$refs.employeeDialog.form)
-              if(res.success){
-                this.getEmployees()
-                this.dialogVisible=false
-                this.$message.success("添加成功")
-              }else{
-                this.$message.error("添加失败")
-              }
-            }
-          })
-      },
-      exportEmployees(){
-          this.downloadLoading = true
-          import('@/utils/Export2Excel').then(excel => {
-            const tHeader = ['序号', '姓名', '手机号', '工号', '聘用形式','部门','入职时间']
-            const filterVal = ['id', 'title', 'author', 'pageviews', 'display_time']
-            const list = this.list
-            const data = this.formatJson(filterVal, list)
-            excel.export_json_to_excel({
-              header: tHeader,
-              data,
-              filename: this.filename,
-              autoWidth: this.autoWidth,
-              bookType: this.bookType
-            })
-            this.downloadLoading = false
-          })
-
-      }
+      employeesList: [], // 员工列表
+      total: 0, // 数据总条数
+      dialogVisible: false,
+      downloading: false
     }
+  },
+  beforeMount() {
+    this.getEmployees(this.query)
+  },
+  methods: {
+    // 每页显示的条数发生改变时触发
+    handleSizeChange(newSize) {},
+
+    // 当前页面发生改变时触发
+    handleCurrentChange(newPage) {},
+    async getEmployees(query) {
+      // 获取员工列表
+      try {
+        const { data: res } = await getEmployeesAPI(query)
+        this.total = res.total
+        this.employeesList = res.rows
+        console.log(res)
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    workNumberSort(a, b) {
+      console.log(a.workNumber - b.workNumber)
+      return a.workNumber - b.workNumber
+    },
+    imgError(e) {
+      e.target.src = 'https://project-waimai.oss-cn-hangzhou.aliyuncs.com/blog/IMG_1515.PNG'
+    },
+    addEmployee() {
+      this.dialogVisible = true
+    },
+    handleClose(done) {
+      this.$confirm('确认关闭？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消'
+      })
+        .then(_ => {
+          done()
+        })
+        .catch(_ => {})
+    },
+    dialogClose() {
+      // 当弹窗关闭时，清空表单
+      this.$nextTick(() => {
+        this.$refs.employeeDialog.$refs.employeeForm.resetFields()
+      })
+    },
+    dialogCancel() {
+      // 弹窗取消按钮触发
+      this.dialogVisible = false
+    },
+    async dialogConfirm() {
+      // 弹窗确认按钮触发
+      console.log(1)
+      this.$refs.employeeDialog.$refs.employeeForm.validate(async valid => {
+        if (valid) {
+          const res = await addEmployeesAPI(this.$refs.employeeDialog.form)
+          if (res.success) {
+            this.getEmployees()
+            this.dialogVisible = false
+            this.$message.success('添加成功')
+          } else {
+            this.$message.error('添加失败')
+          }
+        }
+      })
+    },
+    exportEmployees() {
+      this.downloadLoading = true
+      import('@/utils/Export2Excel').then(async excel => {
+        const tHeader = ['序号', '姓名', '手机号', '工号', '聘用形式', '部门', '入职时间']
+        const res = await getEmployeesAPI({ page: 1, size: this.total })
+        const allList = res.data.rows
+        const data = allList.map((item, index) => {
+          return [index, item.username, item.mobile, item.workNumber, +item.formOfEmployment === 1 ? '正式' : (+item.formOfEmployment === 2 ? '非正式' : '未知'), item.departmentName, item.timeOfEntry]
+        })
+        excel.export_json_to_excel({
+          header: tHeader,
+          data,
+          filename: '员工列表',
+          autoWidth: true,
+          bookType: 'xlsx'
+        })
+        this.downloadLoading = false
+      })
+    }
+  }
 }
 </script>
 
