617653ad3e795bfd0e922a7ce2a385b53668a8a4
wyf043277 <2423839638@qq.com>
2022-12-02 19:36:42 +0800

    20221202 乡下

--- src/views/employees/components/userInfo.vue
+++ src/views/employees/components/userInfo.vue
@@ -12,6 +12,15 @@
           <el-form-item label="手机" prop="mobile">
             <el-input v-model="form.mobile" style="width:80%" placeholder="请输入手机" />
           </el-form-item>
+          <el-form-item label="手机" prop="mobile">
+            <el-upload
+              class="avatar-uploader"
+              action=""
+              :show-file-list="false">
+             <img v-if="imageUrl" :src="imageUrl" class="avatar">
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </el-form-item>
         </div>
         <div class="right">
           <el-form-item label="入职时间" prop="timeOfEntry">
@@ -24,6 +33,11 @@
             <el-input v-model="form.formOfEmployment" style="width:80%" />
           </el-form-item>
         </div>
+
+        <el-form-item>
+          <el-button type="primary" @click="">保存更新</el-button>
+          <el-button @click="">返回</el-button>
+        </el-form-item>
       </el-form>
     </el-card>
   </div>
@@ -43,13 +57,14 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped="scoped">
   .el-card{
     border: none;
   }
   .el-form{
     display: flex;
     justify-content:space-between;
+    flex-wrap:wrap;
   }
   .left{
     width: 50%;
@@ -57,4 +72,22 @@ export default {
   .right{
     width: 50%;
   }
+    .avatar-uploader ::v-deep  .el-upload{
+      border: 1px solid #d9d9d9;
+      border-radius: 6px;
+      cursor: pointer;
+      position: relative;
+      overflow: hidden;
+    }
+      .avatar-uploader .el-upload:hover {
+        border-color: #409EFF;
+      }
+      .avatar-uploader-icon {
+        font-size: 28px;
+        color: #8c939d;
+        width: 178px;
+        height: 178px;
+        line-height: 178px;
+        text-align: center;
+      }
 </style>
