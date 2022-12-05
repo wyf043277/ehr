554b072fcc6904b2ea7b6bfbf55b18a3282b1533
wyf043277 <2423839638@qq.com>
2022-12-05 20:00:20 +0800

    20220512 乡下

--- src/cors/index.js
+++ src/cors/index.js
@@ -1,12 +1,41 @@
 import COS from 'cos-js-sdk-v5'
 import options from './options/option.js'
+var cos = new COS({
+    // getAuthorization 必选参数
+    getAuthorization: function (options, callback) {
+        // 异步获取临时密钥
+        // 服务端 JS 和 PHP 例子：https://github.com/tencentyun/cos-js-sdk-v5/blob/master/server/
+        // 服务端其他语言参考 COS STS SDK ：https://github.com/tencentyun/qcloud-cos-sts-sdk
+        // STS 详细文档指引看：https://cloud.tencent.com/document/product/436/14048
 
-
-// SECRETID 和 SECRETKEY请登录 https://console.cloud.tencent.com/cam/capi 进行查看和管理
-let cos = new COS({
-    SecretId: options.SecretId,
-    SecretKey: options.SecretKey,
-});
+        var url = 'http://127.0.0.1:3000/sts'; // url替换成您自己的后端服务
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onload = function (e) {
+            try {
+                var data = JSON.parse(e.target.responseText);
+                var credentials = data.credentials;
+                console.log(data.credentials)
+            } catch (e) {
+            }
+            if (!data || !credentials) {
+              return console.error('credentials invalid:\n' + JSON.stringify(data, null, 2))
+            };
+            callback({
+              TmpSecretId: credentials.tmpSecretId,
+              TmpSecretKey: credentials.tmpSecretKey,
+              SecurityToken: credentials.sessionToken,
+              // 建议返回服务器时间作为签名的开始时间，避免用户浏览器本地时间偏差过大导致签名错误
+              StartTime: data.startTime, // 时间戳，单位秒，如：1580000000
+              ExpiredTime: data.expiredTime, // 时间戳，单位秒，如：1580000000
+          });
+        };
+        xhr.send();
+    },
+    //配置CDN加速
+    Domain: `ehrcos.wyfjs.top`,// 自定义加速域名，Domain 参数支持模板，这个例子里 {Bucket} 会在请求时按传入的 Bucket 自动替换
+    Protocol: 'http:',// 请求协议： 'https:' 或 'http:'
+})
 function uploadToCos(key,file,onProgress){
   return cos.uploadFile({
     Bucket: options.Bucket,
@@ -16,4 +45,4 @@ function uploadToCos(key,file,onProgress){
     onProgress
   })
 }
-export {cos,uploadToCos}
+export {uploadToCos}
--- src/cors/options/option.js
+++ src/cors/options/option.js
@@ -1,6 +1,4 @@
 let options = {
-  SecretId: 'AKID1ta3IJ8kBQYTDl6D7pJYaPzLbfV5PULa',
-  SecretKey: '6OTKyObeV7WhmgMp68wVL3bw2wd7N1Th',
   Bucket: 'ehr-1312895052',
   Region: 'ap-nanjing',
 }
