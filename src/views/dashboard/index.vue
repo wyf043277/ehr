9672936ca1a503dc5758492fc2a02e428e6409c1
wyf043277 <2423839638@qq.com>
2022-12-14 22:01:44 +0800

    20221214 街上

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -22,9 +22,15 @@
             <div slot="header">
               <span>工作日历</span>
             </div>
-            <div v-for="o in 4" :key="o" class="text item">
-              {{'列表内容 ' + o }}
-            </div>
+            <el-calendar>
+              <template
+                slot="dateCell"
+                slot-scope="{date, data}"
+              >
+                <span :class="data.isSelected ? 'text is-selected' : 'text'">{{ formatDate(date) }}</span>
+                <span v-if="restDay(date)" class="rest">休</span>
+              </template>
+            </el-calendar>
           </el-card>
         </div>
         <div class="mainInfo-right">
@@ -33,7 +39,7 @@
               <span>绩效指数</span>
             </div>
             <div v-for="o in 4" :key="o" class="text item">
-              {{'列表内容 ' + o }}
+              {{ '列表内容 ' + o }}
             </div>
           </el-card>
         </div>
@@ -45,6 +51,7 @@
 <script>
 import { mapGetters } from 'vuex'
 import { getEmployeesBasicInfoAPI } from '@/api'
+import moment from 'moment'
 export default {
   name: 'Dashboard',
   computed: {
@@ -60,6 +67,16 @@ export default {
   async beforeMount() {
     const res = await getEmployeesBasicInfoAPI(this.userInfo.userId)
     this.basicInfo = res.data
+  },
+  methods: {
+    formatDate(date) {
+      return moment(date).format('D')
+    },
+    restDay(date) {
+      if (moment(date).weekday() === 6 || moment(date).weekday() === 0) {
+        return true
+      }
+    }
   }
 }
 </script>
@@ -97,16 +114,70 @@ export default {
 }
 .mainInfo{
   display:flex;
-  justify-content:space-between;
   .box-card{
     padding:5px 10px;
     margin-top:20px;
   }
   .mainInfo-left{
-    flex:1 1 auto;
+    width: 54%;
+    padding-right: 26px;
+    flex: 1;
+    .is-selected{
+      display: inline-block;
+      border-radius: 50%;
+      background-color: #409eff;
+      color: #fff;
+      width: 20px;
+      height: 20px;
+      text-align: center;
+      line-height: 20px;
+    }
+    .text{
+      width: 20px;
+      height: 20px;
+      display: inline-block;
+    }
+    .rest{
+      width: 20px;
+      height: 20px;
+      line-height: 20px;
+      display: inline-block;
+      color: #fff;
+      border-radius: 50%;
+      background-color: #fa7c4d;
+      font-size: 12px;
+      margin-left: 10px;
+    }
   }
-  .mainInfo-right:{
-    flex:1 1 auto;
+  .mainInfo-right{
+    width: 46%;
+    flex: 1;
   }
+}
+
+/* 去除边框 */
+::v-deep .el-calendar-table tr td:first-child{
+  border: none;
+}
+// ::v-deep .el-calendar-table tr:first-child td{
+//   border: none;
+// }
+::v-deep .el-calendar-table__row td{
+  border: none;
+}
+
+::v-deep .el-calendar-day{
+  text-align: center;
+  font-size: 14px;
+  height: auto;
+}
+::v-deep .el-calendar-table thead th {
+    padding: 0;
+    font-size: 16px;
+    color: #606266;
+    font-weight:400;
+}
+::v-deep .el-calendar__header{
+  display: none;
 }
 </style>
